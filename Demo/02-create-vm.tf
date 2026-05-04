de2c1413584ef2e99dadad5cd87e7c240fb6f759
PathiVinayKumar <vinny.pathi@gmail.com>
2026-05-04 16:01:46 +0530

    updated

--- Demo/02-create-vm.tf
+++ Demo/02-create-vm.tf
@@ -14,37 +14,33 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_linux_virtual_machine" "main" {
   name                  = "test-vm"
   location              = "Denmark East"
   resource_group_name   = "Test"
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_D2s_v3"
+  size               = "Standard_D2s_v3"
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
   # delete_data_disks_on_termination = true
 
-  storage_image_id = "/subscriptions/9e27705f-e28f-4f14-9137-ef3f4f8924af/resourceGroups/Test/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0"
+  source_image_id = "/subscriptions/9e27705f-e28f-4f14-9137-ef3f4f8924af/resourceGroups/Test/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0"
 
   os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
   }
 
-  os_profile {
-    computer_name  = "hostname"
     admin_username = "vinay"
     admin_password = "Vinny@123456789"
-  }
+ 
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+   disable_password_authentication = false
+  
 
   # These properties are typically part of a newer resource type (azurerm_linux_virtual_machine)
   # but added here for syntax compatibility with your snippet
   secure_boot_enabled = true
   vtpm_enabled        = true
 }
+ 
