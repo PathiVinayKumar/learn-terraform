833a7a004e177beb2791d9bd69e01bf1883c29aa
PathiVinayKumar <vinny.pathi@gmail.com>
2026-05-04 15:21:47 +0530

    terraform

---
+++ Demo/02-create-vm.tf
@@ -0,0 +1,49 @@
+variable "prefix" {
+  default = "tfvmex"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "test-nic"
+  location            = "Denmark East"
+  resource_group_name = "Test"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "/subscriptions/9e27705f-e28f-4f14-9137-ef3f4f8924af/resourceGroups/Test/providers/Microsoft.Network/virtualNetworks/Monolith-vnet/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "test-vm"
+  location              = "Denmark East"
+  resource_group_name   = "Test"
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard B1s"
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    id = "/subscriptions/9e27705f-e28f-4f14-9137-ef3f4f8924af/resourceGroups/Test/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0"
+
+  }
+
+  storage_os_disk {
+    name              = "test-vm"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "test-vm"
+    admin_username = "vinay"
+    admin_password = "Vinny@123456789"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
