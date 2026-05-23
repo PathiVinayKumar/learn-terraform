600446d10b1876b8046a761112bad17266e88b9f
PathiVinayKumar <vinny.pathi@gmail.com>
2026-05-23 16:57:58 +0530

    changes

--- Demo2/main.tf
+++ Demo2/main.tf
@@ -1,9 +1,41 @@
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
-  location = "West Europe"
+resource "azurerm_network_interface" "main" {
+  name                = "test-nic"
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "/subscriptions/9e27705f-e28f-4f14-9137-ef3f4f8924af/resourceGroups/Test/providers/Microsoft.Network/virtualNetworks/Monolith-vnet/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                  = "test-vm"
+  location              = "Denmark East"
+  resource_group_name   = "denmark-east-rg"
+  network_interface_ids = [azurerm_network_interface.main.id]
+  size               = "Standard_D2s_v3"
+
+  source_image_id = "/subscriptions/9e27705f-e28f-4f14-9137-ef3f4f8924af/resourceGroups/Test/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_password = "Vinny@123456789"
+  admin_username = "vinay"
+
+  disable_password_authentication = false
+
+  secure_boot_enabled = true
+  vtpm_enabled        = true
+
 }
